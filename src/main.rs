cc3d7a17fdaf4aef47694e92ab958f00098a8180
Benn1x <benn1x@daskju.com>
2023-03-20 20:47:19 +0100

    init commit

--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,27 @@
 use std::fs::read_to_string;
+use std::fs::File;
+use std::io::prelude::*;
 use std::io::{stdin, Read};
 
 fn main() {
     let args: Vec<String> = std::env::args().collect();
-    for pos in 1..args.len() {
-        execute(&*args[pos]);
+    for pos in 2..args.len() {
+        match &*args[1] {
+            "-b" => build_bin(&args[pos]),
+
+            _ => execute(&args[pos]),
+        }
     }
 }
 
 fn read_in(path: &str) -> String {
-    let file = match read_to_string(path) {
+    match read_to_string(path) {
         Ok(val) => val,
         Err(err) => {
             eprintln!("Err: {}", err);
             std::process::exit(1);
         }
-    };
-    file
+    }
 }
 
 fn execute(path: &str) {
@@ -90,5 +95,73 @@ fn execute(path: &str) {
             println!("output: {}", s);
             break;
         }
+    }
+}
+
+fn build_bin(path: &str) {
+    let file = read_in(path);
+    let mut stack = Vec::<char>::new();
+    let chars: Vec<char> = file.chars().collect();
+    let mut bytecode = Vec::<&str>::new();
+    let mut progr = 0;
+
+    loop {
+        match chars[progr] {
+            '.' => bytecode.push("WRITE"),
+            ',' => bytecode.push("INPUT"),
+            '<' => bytecode.push("NEXT"),
+            '>' => bytecode.push("BACKE"),
+            '+' => bytecode.push("INCREASE"),
+            '-' => bytecode.push("DECREASE"),
+            '[' => {
+                bytecode.push("FOR");
+                stack.push('[');
+                let mut unmatch = progr + 1;
+                let mut deep = 1;
+                loop {
+                    match chars[unmatch] {
+                        '[' => deep += 1,
+                        ']' => deep -= 1,
+                        _ => (),
+                    }
+                    unmatch += 1;
+                    if deep == 0 {
+                        break;
+                    }
+                    if unmatch >= chars.len() {
+                        panic!("Unmatched ]");
+                    }
+                }
+            }
+            ']' => {
+                let i = stack.last();
+                if let Some('[') = i {
+                    stack.pop();
+                    bytecode.push("END");
+                } else {
+                    eprintln!("Unmatched ']'");
+                    std::process::exit(1);
+                }
+            }
+            _ => (),
+        }
+
+        progr += 1;
+        if progr >= chars.len() {
+            break;
+        }
+    }
+    let mut out_file = match File::create(format!("{}.bbf", path)) {
+        Ok(val) => val,
+        Err(_) => match File::open(format!("{}.bbf", path)) {
+            Ok(val) => val,
+            Err(err) => {
+                panic!("{}", err);
+            }
+        },
+    };
+    for instr in bytecode {
+        out_file.write_all(instr.as_bytes()).unwrap();
+        out_file.write_all(b"\n").unwrap();
     }
 }
