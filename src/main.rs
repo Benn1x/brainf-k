e967517aab2c74f7570c8ad341adee6ea27df526
Benn1x <benn1x@daskju.com>
2023-04-14 11:26:42 +0200

    input/output work perfectly fine

--- src/main.rs
+++ src/main.rs
@@ -398,13 +398,13 @@ impl LLVM {
                 LLVMInt32TypeInContext(self.ctx),
                 &mut LLVMVoidTypeInContext(self.ctx),
                 0,
-                1,
+                0,
             );
             let putchar_head = LLVMFunctionType(
                 LLVMInt32TypeInContext(self.ctx),
-                &mut LLVMVoidTypeInContext(self.ctx),
-                0,
+                &mut LLVMInt32TypeInContext(self.ctx),
                 1,
+                0,
             );
             let getchar = LLVMAddFunction(self.module, cstr("getchar").as_ptr(), getchar_head);
             let putchar = LLVMAddFunction(self.module, cstr("putchar").as_ptr(), putchar_head);
@@ -605,7 +605,7 @@ impl LLVM {
                     }
 
                     b'.' => {
-                        let mut elem_ptr = LLVMBuildGEP2(
+                        let elem_ptr = LLVMBuildGEP2(
                             self.builder,
                             LLVMPointerType(LLVMInt8TypeInContext(self.ctx), 0),
                             arr,
@@ -613,11 +613,17 @@ impl LLVM {
                             0,
                             cstr("").as_ptr(),
                         );
+                        let mut trunc = LLVMBuildLoad2(
+                            self.builder,
+                            LLVMInt32TypeInContext(self.ctx),
+                            elem_ptr,
+                            cstr("").as_ptr(),
+                        );
                         LLVMBuildCall2(
                             self.builder,
                             putchar_head,
                             putchar,
-                            &mut elem_ptr,
+                            &mut trunc,
                             1,
                             cstr("").as_ptr(),
                         );
@@ -698,7 +704,7 @@ impl LLVM {
                 cstr("").as_ptr(),
                 llvm_sys::target_machine::LLVMCodeGenOptLevel::LLVMCodeGenLevelAggressive,
                 llvm_sys::target_machine::LLVMRelocMode::LLVMRelocDefault,
-                llvm_sys::target_machine::LLVMCodeModel::LLVMCodeModelDefault,
+                llvm_sys::target_machine::LLVMCodeModel::LLVMCodeModelSmall,
             );
             match LLVMTargetMachineEmitToFile(
                 target_machine,
